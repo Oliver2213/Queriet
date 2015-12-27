398219cdf955960fa763cf1c512626a48d088b00
blake oliver <oliver22213@me.com>
2015-12-27 11:30:43 -0500

    Refactor UI.py to store all items in it's class instance
    Probably doesn't matter but... Ehh, I'm picky.

--- queriet/ui.py
+++ queriet/ui.py
@@ -4,6 +4,7 @@
 import wx
 
 class mainUI(wx.Frame):
+	"""Class that holds the main user interface for Queriet"""
 	def __init__(self, parent, title):
 		super(mainUI, self).__init__(parent, title=title, size=(1000, 800))
 		self.setup()
@@ -11,53 +12,55 @@ class mainUI(wx.Frame):
 		self.Show()
 
 	def setup(self):
-		panel = wx.Panel(self) # the main pannel that children pannels inherit from
+		"""Sets up the application UI layout"""
+		self.panel = wx.Panel(self) # the main pannel that children pannels inherit from
 
 		#API list
-		listPanel = wx.Panel(panel)
-		listSizer = wx.BoxSizer(wx.HORIZONTAL) #A list to hold the API listview
-		apiStatic = wx.StaticText(listPanel, -1, 'API') #A label for our listview
-		listSizer.Add(apiStatic, 1, wx.TOP | wx.BOTTOM | wx.LEFT, 5) # adding this label to our api sizer
-		self.apiList = wx.ListBox(listPanel, -1) #create the actual list
-		listSizer.Add(self.apiList, 1, wx.EXPAND | wx.ALL, 20) #Add it to the list sizer
+		self.listPanel = wx.Panel(self.panel)
+		self.listSizer = wx.BoxSizer(wx.HORIZONTAL) #A list to hold the different APIs
+		self.apiStatic = wx.StaticText(self.listPanel, -1, 'API') #A label for our listview
+		self.listSizer.Add(self.apiStatic, 1, wx.TOP | wx.BOTTOM | wx.LEFT, 5) # adding this label to our api sizer
+		self.apiList = wx.ListBox(self.listPanel, -1) #create the actual list
+		self.listSizer.Add(self.apiList, 1, wx.EXPAND | wx.ALL, 20) #Add it to the list sizer
 
-		#Info panel
-		infoPanel = wx.Panel(panel)
-		infoSizer = wx.BoxSizer(wx.VERTICAL)
+		#Info panel, holds input and output pannels
+		self.infoPanel = wx.Panel(self.panel)
+		self.infoSizer = wx.BoxSizer(wx.VERTICAL)
 
-		#Input panel.
-		inputPanel = wx.Panel(infoPanel)
-		inputStatic = wx.StaticText(inputPanel, -1, 'Search term or equation')
-		self.input = wx.TextCtrl(inputPanel, -1)
-		self.searchButton = wx.Button(inputPanel, -1, 'Search', size=(90, 30))
-		inputSizer = wx.BoxSizer(wx.HORIZONTAL)
-		inputSizer.Add(inputStatic, 1, wx.TOP|wx.LEFT|wx.BOTTOM, 5)
-		inputSizer.Add(self.input, 3, wx.TOP|wx.BOTTOM, 10)
-		inputSizer.Add(self.searchButton, 2, wx.TOP|wx.RIGHT|wx.BOTTOM, 20)
+		#Input panel, used for inputting information for a query, can be API defined
+		self.inputPanel = wx.Panel(self.infoPanel)
+		self.inputStatic = wx.StaticText(self.inputPanel, -1, 'Search term or equation')
+		self.input = wx.TextCtrl(self.inputPanel, -1)
+		self.searchButton = wx.Button(self.inputPanel, -1, 'Search', size=(90, 30))
+		self.inputSizer = wx.BoxSizer(wx.HORIZONTAL) # A sizer for items in the input panel
+		self.inputSizer.Add(self.inputStatic, 1, wx.TOP|wx.LEFT|wx.BOTTOM, 5) # adding out input label
+		self.inputSizer.Add(self.input, 3, wx.TOP|wx.BOTTOM, 10)
+		self.inputSizer.Add(self.searchButton, 2, wx.TOP|wx.RIGHT|wx.BOTTOM, 20)
 
 		#Output panel
-		outputPanel = wx.Panel(infoPanel)
-		outputSizer = wx.BoxSizer(wx.VERTICAL)
-		outputStatic = wx.StaticText(outputPanel, -1, 'results', (5, 5))
-		self.output = wx.TextCtrl(outputPanel, -1, style=wx.TE_MULTILINE|wx.TE_READONLY)
-		outputSizer.Add(outputStatic, 0, wx.TOP|wx.LEFT, 10)
-		outputSizer.Add(self.output, 6, wx.LEFT|wx.BOTTOM|wx.RIGHT|wx.EXPAND, 20)
+		self.outputPanel = wx.Panel(self.infoPanel)
+		self.outputSizer = wx.BoxSizer(wx.VERTICAL)
+		self.outputStatic = wx.StaticText(self.outputPanel, -1, 'results', (5, 5))
+		self.output = wx.TextCtrl(self.outputPanel, -1, style=wx.TE_MULTILINE|wx.TE_READONLY)
+		self.outputSizer.Add(self.outputStatic, 0, wx.TOP|wx.LEFT, 10)
+		self.outputSizer.Add(self.output, 6, wx.LEFT|wx.BOTTOM|wx.RIGHT|wx.EXPAND, 20)
 
-		infoSizer.Add(inputPanel, 1, wx.TOP|wx.LEFT|wx.RIGHT, 20)
-		infoSizer.Add(outputPanel, 3, wx.LEFT|wx.BOTTOM|wx.RIGHT|wx.EXPAND, 30)
+		#Add our input and output pannels to the info panel sizer
+		self.infoSizer.Add(self.inputPanel, 1, wx.TOP|wx.LEFT|wx.RIGHT, 20)
+		self.infoSizer.Add(self.outputPanel, 3, wx.LEFT|wx.BOTTOM|wx.RIGHT|wx.EXPAND, 30)
 
 
 		#main
-		mainSizer = wx.BoxSizer(wx.HORIZONTAL)
-		mainSizer.Add(listPanel, 0, wx.TOP | wx.BOTTOM | wx.LEFT, 30)
-		mainSizer.Add(infoPanel, 5, wx.EXPAND|wx.ALL, 30)
+		self.mainSizer = wx.BoxSizer(wx.HORIZONTAL)
+		self.mainSizer.Add(self.listPanel, 0, wx.TOP | wx.BOTTOM | wx.LEFT, 30)
+		self.mainSizer.Add(self.infoPanel, 5, wx.EXPAND|wx.ALL, 30)
 
 		#Setting sizers
-		panel.SetSizer(mainSizer)
-		listPanel.SetSizer(listSizer)
-		inputPanel.SetSizer(inputSizer)
-		outputPanel.SetSizer(outputSizer)
-		infoPanel.SetSizer(infoSizer)
+		self.panel.SetSizer(self.mainSizer)
+		self.listPanel.SetSizer(self.listSizer)
+		self.inputPanel.SetSizer(self.inputSizer)
+		self.outputPanel.SetSizer(self.outputSizer)
+		self.infoPanel.SetSizer(self.infoSizer)
 
 
 
