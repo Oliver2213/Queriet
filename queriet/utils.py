52849e5c8bb1cc9df54ad579b8fbfcbed746ab05
blake oliver <oliver22213@me.com>
2015-12-28 00:51:28 -0500

    Create utils.py for misc utility functions, add CreateMenuItem to utils
    Call this like...
    menu=wx.Menu()
    utils.CreateMenuItem(menu, 'some item', some_item_function) # Creates a menu item with the specified label (you can optionally pass position), and binds it to the specified function. Then adds that item to the specified menu.
    The order in which you add items to any menu in this manner is importand unless you specify an explicit position, as by default, items get appended to the end of the menu you pass in.

---
+++ queriet/utils.py
@@ -0,0 +1,16 @@
+#Utility functions for Queriet
+#Please see the license file in the main directory of this repository for such information
+
+import wx
+def CreateMenuItem(menu, label, position=-1, func):
+	"""A quick function to add and bind a menu item.
+		This can be used for menubars, system tray icons, etc.
+		Just pass it the menu object in question, a label for your new option, it's position (-1 by default, which appends to end), and a funcion you'd like to bind it to.
+		By default, this function appends your item to the end of the menu, so the order in which you add items by calling this function is important to how the menu looks.
+		Also, remember you can denote a shortcut key with the and (&) sign before the letter in the label.
+		Coppied and slightly modiefied from http://stackoverflow.com/questions/6389580/quick-and-easy-trayicon-with-python
+	"""
+	item = wx.MenuItem(menu, position, label)
+	menu.Bind(wx.EVT_MENU, func, id=item.GetId())
+	menu.AppendItem(item)
+	return item
