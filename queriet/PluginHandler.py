0641d975285cc86222ea471764f28e7069c31286
blake oliver <oliver22213@me.com>
2016-01-02 00:22:01 -0500

    Rename APIPluginHandler to PluginHandler, refactor said code for yapsy, update controller with methods for plugins
    Now, the list of currently activated plugins is automatically obtained on controller instantiation and stored in the self.plugins dict, keyed by plugin name.
    So to run a method for each plugin, (in controller), you would do:
    for plugin in self.plugins:
    	plugin.SayHi()
    
    To refresh the list of plugins, (maybe after activation / deactivation of some, removal of others), in controller, run self.BuildPluginList()

---
+++ queriet/PluginHandler.py
@@ -0,0 +1,40 @@
+#This is the API handler of Queriet
+#Please see the license file in the main directory of this repository for licensing information
+
+import yapsy
+
+class Plugin(yapsy.IPlugin.IPlugin):
+	"""This class is a template for a plugin. 
+		We use yapsy for plugin management, so you need a basic ini config file with a .queriet-plugin extention, looking something like:
+		[Core]
+		Name = the API plugin's name. Usually this can be set to the name of the service you want this to handle, such as "wikipedia"
+		Module = your_plugin_filename_without_extentionoes. Usually something like, "Searches twitter for tweets with your searchterm in them", will suffice.
+		[Documentation]
+		Version = the version of your APIPlugin. Integers only, please.
+		Author = Author(s) of your plugin. E.G. author = some dude <some-dude@dudes.net>, Some Girl <some-girl@girls.net>
+		Website = http://example.com/your_plugin
+		
+		Your APIPlugin will also need to support the following methods:
+		getResult(searchtext) - This is the main one. This needs to return a Queriet result object, or a list of them. Whatever your APIPlugin needs to do (Look something up online, calculate a math problem, compute a file hash), you need to kick it off and return it here. It generally needs to be fast, too. In future there will probably be a setting to have Queriet begin caching results for each api as soon as the go button is pressed.
+		inputPanel - This is a method that will be passed the "infoPanel" as a parrent. It is expected that this is instantiated like, wx.Panel(infoPanel). You can use this to change input your plugin receives. Also, PLEASE USE SIZERS!
+		outputPanel - same as input, but deals with displaying results of a search. Maybe you want a big text field, a picture and a text field, etc etc.
+	"""
+
+	#Should be the same name as your module name
+	name="no name"
+	def activate():
+		"""Your plugin should run any code necessary to get ready to deal with searches or other operations your plugins do. Bind hotkeys, authorize to an API, whatever."""
+		pass
+
+	def deactivate():
+		"""Your plugin needs to release any resources it may have acquired, unbind any hotkeys, clear any caches, etc..."""
+		pass
+
+	#The input and output panels are None by default, redefine them in your plugin to have the main UI use them
+	InputPanel=None
+	OutputPanel=None
+
+def GetPluginManager():
+	"""Returns a plugin manager object, with Queriet-specific settings."""
+	pm=yapsy.PluginManager.PluginManager(directories_list=['plugins'], plugin_info_ext='queriet-plugin')
+	return pm
--- queriet/controller.py
+++ queriet/controller.py
@@ -3,15 +3,28 @@
 #This file handles overall Queriet operations
 
 from ui import MainUI
+import PluginHandler
 
 class Controller(object):
 	def __init__(self, application):
 		self.application = application
 		self.setup()
+		self.BuildPluginList()
 
 	def setup(self):
 		"""Create a frame for the main UI and link it to the controller, as well as setting up a plugin handler."""
 		self.ui = MainUI(self, None, "Queriet")
+		self.pm = PluginHandler.GetPluginManager()
+		self.pm.collectPlugins()
+		for plugin in self.pm.getAllPlugins:
+			self.pm.activatePluginByName(plugin.name)
+
+	def BuildPluginList():
+		"""Build, or (re)build, the list of currently activated plugins, and store them in the dict self.plugins, keyed by name."""
+		if self.pm: # we have a plugin manager object
+			self.plugins={}
+			for plugin in self.pm.getAllPlugins():
+				self.plugins[plugin.name]=plugin.plugin_object
 
 	def run(self):
 		"""Begin the main application loop, if applicable."""
