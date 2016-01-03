fd2664908f5189d722eae3bf5624668b734ad1ae
blake oliver <oliver22213@me.com>
2016-01-03 15:59:06 -0500

    Finish up UI's 'AddPluginsToList' method.
    This method will automatically be called from controller's 'BuildPluginList', to populate the API listbox in the UI.

--- queriet/ui.py
+++ queriet/ui.py
@@ -86,13 +86,13 @@ class MainUI(wx.Frame):
 		"""Opens the Queriet website"""
 		os.startfile('https://github.com/oliver2213/queriet')
 
-	def AddPluginsToList()
+	def AddPluginsToList(self):
 		"""This method adds each plugin found in self.controller.plugins to lists that need them, currently only the main listbox.
 			This method gets run each time the plugins list is rebuilt."""
 		if self.apiList.IsEmpty()==False:
 			self.apiList.Clear()
 		for plugin in self.controller.plugins:
-			self.apiList.Append(plugin.name, self.controller
+			self.apiList.Append(plugin.name, plugin)
 
 	def OnClose(self, event):
 		"""Delete system tray icon and this window."""
