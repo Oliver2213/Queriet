877979fa06f4b2815f08bbbd64b75ba432a9aefb
blake oliver <oliver22213@me.com>
2016-01-04 11:59:02 -0500

    Update docstrings for the Plugin class of PluginHandler

--- queriet/PluginHandler.py
+++ queriet/PluginHandler.py
@@ -1,4 +1,4 @@
-#This is the API handler of Queriet
+#This is the plugin handler of Queriet
 #Please see the license file in the main directory of this repository for licensing information
 
 import wx
@@ -12,7 +12,7 @@ class Plugin(IPlugin):
 		Name = the API plugin's name. Usually this can be set to the name of the service you want this to handle, such as "wikipedia"
 		Module = your_plugin_filename_without_extentions. Usually something like, "Searches twitter for tweets with your searchterm in them", will suffice.
 		[Documentation]
-		Version = the version of your APIPlugin. Integers only, please.
+		Version = the version of your plugin.
 		Author = Author(s) of your plugin. E.G. author = some dude <some-dude@dudes.net>, Some Girl <some-girl@girls.net>
 		Website = http://example.com/your_plugin
 		
@@ -30,6 +30,7 @@ class Plugin(IPlugin):
 		self.controller = controller
 
 	def setup(self):
+		"""This would be a good place to assign your InputPanel and OutputPanels, if your using either of these"""
 		pass
 
 	def activate(self):
@@ -41,12 +42,19 @@ class Plugin(IPlugin):
 		pass
 
 	def on_gain_focus(self):
+		"""Unless there is something special that your plugin needs to do when it is selected in the API listbox in the user interface, you probably shouldn't mess with this method.
+			If, however, your plugin needs to start pulling data from the web (maybe to display in a list for a "top news" plugin), you should do that here by using Super to get what is defined here (which is the code to run the OnGainFocus and OnLoseFocus methods for your input and output panels).
+		"""
 		if self.InputPanel:
 			self.InputPanel.on_gain_focus()
 		if self.OutputPanel:
 			self.OutputPanel.on_gain_focus()
 
 	def on_lose_focus(self):
+		"""This method runs when your plugin loses it's selection in the main UI API listbox. If you have some data cached in memory, (say a previous search), you might wanna go ahead and write it to disk, close any Http / https sessions, that sort of thing.
+			Your plugin should not, however, release any resources it might have acquired, such as keybindings, temporary API tokens, etc - losing listbox focus just means the user is using (or might use, since they selected) another API in the list. Use the Deactivate method to release resources.
+			Remember to use Super to get the functionality defined here, which just calls the OnLoseFocus method of your panels.
+		"""
 		if self.InputPanel:
 			self.InputPanel.on_lose_focus()
 		if self.OutputPanel:
@@ -60,9 +68,11 @@ class pluginPanel(wx.Panel):
 	- An on_gain_focus and on_lose_focus method. These will instantiate and change whatever they need to for the panels when they gain and lose focus when plugins change, respectively."""
 
 	def on_gain_focus(self):
+		"""This method should do things that are *specific* to the panel when it gets focus. This might be loading up previous searches from a config file, setting controls to their last position, etc."""
 		pass
 
 	def on_lose_focus(self):
+		"""Again, this method needs to do things that are *specific* to the panel - the plugin's OnGainFocus and OnLoseFocus methods will handle everything else."""
 		pass
 
 def GetPluginManager():
