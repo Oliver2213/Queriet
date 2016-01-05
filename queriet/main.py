655cc262b7c1d45db16f4955e56385c2f67fef47
blake oliver <oliver22213@me.com>
2016-01-04 23:05:55 -0500

    Add basic logging to main
    Currently, log messages go to two places:
    * sys.stderr (the console window, this will be disabled once we start compiling Queriet)
    * Queriet.log (this doesn't rotate or get wiped upon each queriet startup though it might in the future)

--- queriet/main.py
+++ queriet/main.py
@@ -1,10 +1,23 @@
-#Queriet main application launch file
+#Queriet main application file
 #Please see the license file in the main directory for licensing information
 
+__version__=0.1
+__author__='Blake Oliver <oliver22213@me.com>, Bradley Renshaw <bradjrenshaw@gmail.com>'
 
+import logging
 import wx
 from controller import Controller
 
+log = logging.getLogger(__name__)
+LogFormat = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+ConsoleHandler = logging.StreamHandler()
+ConsoleHandler.setLevel(logging.DEBUG)
+ConsoleHandler.setFormatter(formatter)
+FileHandler = logging.FileHandler('Queriet.log')
+FileHandler.setLevel(logging.DEBUG)
+FileHandler.setFormatter(LogFormat)
+log.addHandler(ConsoleHandler)
+log.addHandler(FileHandler)
 app = wx.App()
 MainController = Controller(app)
 MainController.run()
