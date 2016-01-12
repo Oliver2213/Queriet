053ca7c7e145617f313003064a29685f2e348887
blake oliver <oliver22213@me.com>
2016-01-12 01:30:30 -0500

    Add logging to main UI

--- queriet/ui.py
+++ queriet/ui.py
@@ -1,14 +1,18 @@
 #Queriet user interface file
 #Please see the license file in the main directory for licensing information
 
+import logging
 import os
 import wx
 import utils
 import info
+
 class MainUI(wx.Frame):
 	"""Class that holds the main user interface for Queriet"""
 	def __init__(self, controller, parent, title):
 		super(MainUI, self).__init__(parent, title=title, size=(1000, 800))
+		self.log = logging.getLogger('Queriet.'+__name__)
+		self.log.info("User interface starting up.")
 		self.controller = controller
 		self.setup()
 		self.SetupMenuBar()
@@ -19,7 +23,7 @@ class MainUI(wx.Frame):
 
 	def setup(self):
 		"""Sets up the application UI layout and menu bar"""
-
+		self.log.debug("Setting up main UI...")
 		self.CurrentPlugin = None
 		self.CurrentPluginNumber = -1
 		self.InfoPanel = None
@@ -43,28 +47,34 @@ class MainUI(wx.Frame):
 		self.listPanel.SetSizer(self.listSizer)
 		self.MainSizer.Layout()
 		self.Fit()
+		self.log.debug("Done!")
 
 	def SetupMenuBar(self):
 		"""Setup our menubar.
 			Menu bar menus as well as items should be named like: self.MenuBar_file (the file, menu), self.MenuBar_file_exit (an exit item in the file menu)
 		"""
-		self.MenuBar = wx.MenuBar()
-		self.MenuBar_file = wx.Menu()
-		self.MenuBar_file_exit = utils.CreateMenuItem(self.MenuBar_file, 'E&xit', self.controller.OnClose, id=wx.ID_EXIT)
-		
-		self.MenuBar.Append(self.MenuBar_file, '&File')
-		self.MenuBar_help = wx.Menu()
-		self.MenuBar_help_about = utils.CreateMenuItem(self.MenuBar_help, "&About...", self.OnAbout, id=wx.ID_ABOUT)
-		self.MenuBar_help_OpenSite = utils.CreateMenuItem(self.MenuBar_help, 'Open Queriet &site...', self.OpenSite)
-		self.MenuBar_help_OpenIssuePage = utils.CreateMenuItem(self.MenuBar_help, 'File an &issue...', self.OpenIssuePage)
-		self.MenuBar.Append(self.MenuBar_help, '&Help')
-
-		self.SetMenuBar(self.MenuBar)
-
+		self.log.debug("Setting up menu bar...")
+		try:
+			self.MenuBar = wx.MenuBar()
+			self.MenuBar_file = wx.Menu()
+			self.MenuBar_file_exit = utils.CreateMenuItem(self.MenuBar_file, 'E&xit', self.controller.OnClose, id=wx.ID_EXIT)
+			
+			self.MenuBar.Append(self.MenuBar_file, '&File')
+			self.MenuBar_help = wx.Menu()
+			self.MenuBar_help_about = utils.CreateMenuItem(self.MenuBar_help, "&About...", self.OnAbout, id=wx.ID_ABOUT)
+			self.MenuBar_help_OpenSite = utils.CreateMenuItem(self.MenuBar_help, 'Open Queriet &site...', self.OpenSite)
+			self.MenuBar_help_OpenIssuePage = utils.CreateMenuItem(self.MenuBar_help, 'File an &issue...', self.OpenIssuePage)
+			self.MenuBar.Append(self.MenuBar_help, '&Help')
+			self.SetMenuBar(self.MenuBar)
+		except:
+			self.log.exception("Error setting up menu bar!")
+		self.log.debug("Done!")
 
 	def SetInfoPanel(self, info):
 		"""This method sets the info panel object."""
+		self.log.debug("Setting the info panel.\nProvided panel is %s" %(info))
 		if self.InfoPanel:
+			self.log.debug("Existing info panel %s, hiding it." %(self.InfoPanel))
 			self.InfoPanel.Hide()
 		self.MainSizer.Clear()
 		self.MainSizer.Add(self.listPanel, 1)
@@ -72,10 +82,13 @@ class MainUI(wx.Frame):
 		self.InfoPanel = info
 		self.MainSizer.Layout()
 		self.Fit()
+		self.log.debug("Set new info panel.")
 
 	def CreateIcon(self):
 		"""Creates the system tray icon."""
+		self.log.debug("Creating system tray icon...")
 		self.icon = SystemTrayIcon(UI=self, text="Queriet")
+		self.log.debug("Done!")
 
 	def SetFocus(self, value):
 		"""Change the displayed UI to that of the selected plugin. Do not pass this function a value less than 0."""
@@ -94,8 +107,10 @@ class MainUI(wx.Frame):
 	def showhide(self, event=None):
 		if self.Shown:
 			self.Hide()
+			self.log.debug("Hid main window.")
 		else:
 			self.Show()
+			self.log.debug("Showed window.")
 
 	def OpenSite(self, event):
 		"""Opens the Queriet website"""
@@ -107,15 +122,22 @@ class MainUI(wx.Frame):
 	def AddPluginsToList(self):
 		"""This method adds each plugin found in self.controller.plugins to lists that need them, currently only the main listbox.
 			This method gets run each time the plugins list is rebuilt."""
+		self.log.debug("Adding plugins to the UI list...")
 		if self.apiList.IsEmpty()==False:
+			self.log.debug("Plugin list already has items, clearing.")
 			self.apiList.Clear()
-		for plugin_info, plugin_object in self.controller.plugins.iteritems():
-			self.apiList.Append(plugin_info.name, plugin_object)
+		try:
+			for plugin_info, plugin_object in self.controller.plugins.iteritems():
+				self.apiList.Append(plugin_info.name, plugin_object)
+		except:
+			self.log.exception("Error adding plugin to list!")
+		self.log.debug("Done!")
 
 	def OnListChange(self, event):
 		sel = self.apiList.GetSelection()
 		if sel < 0:
 			return
+		self.log.debug("API list selection changed.\n%s is now selected." %(cell))
 		self.SetFocus(sel)
 
 	def OnAbout(self, event):
@@ -124,9 +146,12 @@ class MainUI(wx.Frame):
 
 	def OnClose(self, event):
 		"""Delete system tray icon and this window."""
-		self.icon.Destroy()
-		self.Destroy()
-
+		self.log.debug("Closing UI...")
+		try:
+			self.icon.Destroy()
+			self.Destroy()
+		except:
+			self.log.exception("Error closing UI!")
 
 class SystemTrayIcon(wx.TaskBarIcon):
 	"""Class that implements a system tray icon for Queriet"""
@@ -134,6 +159,7 @@ class SystemTrayIcon(wx.TaskBarIcon):
 	def __init__(self, UI, text):
 		"""This is the initialization for the system tray icon class. It creates the menus for use in CreatePopupMenu, and PopupMenu. It also gets passed the MainUI object so it can call it's methods for menu items."""
 		super(SystemTrayIcon, self).__init__()
+		self.log = logging.getLogger('Queriet.'+__name__)
 		self.MUI = UI
 		self.SetIcon(wx.NullIcon, text)
 		self.CreateMenu()
@@ -141,15 +167,20 @@ class SystemTrayIcon(wx.TaskBarIcon):
 
 	def CreateMenu(self):
 		#Create our menu now, so we can reuse it later
-		self.menu = wx.Menu()
-		self.showhide_item = utils.CreateMenuItem(self.menu, '&show or hide Queriet', self.MUI.showhide)
-		self.openSite_item = utils.CreateMenuItem(self.menu, 'Open the Queriet &website', self.MUI.OpenSite)
-		self.about = utils.CreateMenuItem(self.menu, 'About...', self.MUI.OnAbout, id=wx.ID_ABOUT)
-		self.menu.AppendSeparator()
-		self.exit_item = utils.CreateMenuItem(self.menu, 'e&xit', self.MUI.controller.OnClose, id=wx.ID_EXIT)
+		self.log.debug("Creating system tray menu...")
+		try:
+			self.menu = wx.Menu()
+			self.showhide_item = utils.CreateMenuItem(self.menu, '&show or hide Queriet', self.MUI.showhide)
+			self.openSite_item = utils.CreateMenuItem(self.menu, 'Open the Queriet &website', self.MUI.OpenSite)
+			self.about = utils.CreateMenuItem(self.menu, 'About...', self.MUI.OnAbout, id=wx.ID_ABOUT)
+			self.menu.AppendSeparator()
+			self.exit_item = utils.CreateMenuItem(self.menu, 'e&xit', self.MUI.controller.OnClose, id=wx.ID_EXIT)
+		except:
+			self.log.debug("Done!")
 
 	def CreatePopupMenu(self):
 		"""Show the menu."""
+		self.log.debug("Pop up system tray menu.")
 		self.PopupMenu(self.menu)
 
 	def on_left_down(self, event):
@@ -157,4 +188,8 @@ class SystemTrayIcon(wx.TaskBarIcon):
 		self.MUI.showhide(None) # it expects to be passed an event object, so we use none
 
 	def OnClose(self, event):
-		wx.CallAfter(self.MUI.controller.close) #Run the top-level close method instead of just the UI's one, so other resources can be released if needed
+		self.log.debug("Closing Queriet from the system tray.")
+		try:
+			wx.CallAfter(self.MUI.controller.close) #Run the top-level close method instead of just the UI's one, so other resources can be released if needed
+		except:
+			self.log.exception("Error closing Queriet from system tray!")
