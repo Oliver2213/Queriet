092c045708c6175a1dbaa1193d6276c4b64b5920
blake oliver <oliver22213@me.com>
2016-01-15 18:57:45 -0500

    Update the ReadOnlyPanel in default panels a bit

--- queriet/defaultPanels.py
+++ queriet/defaultPanels.py
@@ -30,8 +30,8 @@ class ReadOnlyOutputPanel(wx.Panel):
 		self.outputSizer = wx.BoxSizer(wx.VERTICAL)
 		self.outputStatic = wx.StaticText(self, -1, 'results', (5, 5))
 		self.output = wx.TextCtrl(self, -1, style=wx.TE_MULTILINE|wx.TE_READONLY)
-		self.outputSizer.Add(self.outputStatic, 0, wx.TOP|wx.LEFT, 10)
-		self.outputSizer.Add(self.output, 6, wx.LEFT|wx.BOTTOM|wx.RIGHT|wx.EXPAND, 20)
+		self.outputSizer.Add(self.outputStatic, 1, wx.TOP|wx.LEFT, 10)
+		self.outputSizer.Add(self.output, 3, wx.LEFT|wx.BOTTOM|wx.RIGHT|wx.EXPAND, 20)
 		self.SetSizer(self.outputSizer)
 		self.Hide()
 
