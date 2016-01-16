075c1e61f67ff604acf4c9c2f2c5a6f155a88b03
blake oliver <oliver22213@me.com>
2016-01-16 11:06:31 -0500

    version is now 0.2

--- queriet/info.py
+++ queriet/info.py
@@ -1,3 +1,3 @@
 #Really hacky way to do this, but ehh
-version = 0.1
+version = 0.2
 authors = 'Blake Oliver <oliver22213@me.com>, Bradley Renshaw <bradjrenshaw@gmail.com>, Dhruv kumar <dhruvdropbox@gmail.com>'
