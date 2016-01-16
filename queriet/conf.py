c1f7d6c0600a5e742e945cd3193b9b9c1e5603c5
blake oliver <oliver22213@me.com>
2016-01-16 01:12:52 -0500

    Add error handling to conf.py, change imports to only get what conf uses instead of getting configobj and validator

--- queriet/conf.py
+++ queriet/conf.py
@@ -1,16 +1,28 @@
 #Queriet config module
 
-import configobj
-import validate
+#We don't need both modules, just a few things from each
+from configobj import ConfigObj, ParseError
+from validate import Validator, ValidateError
+
+
+#Our catch-all ConfError exception class
+
+class ConfError(Exception): pass
 
 def GetConfig(specpath, configpath):
 	"""This function loads a config (if found), checks it against the provided specification, and returns it"""
-	#Load the provided specification
-	spec = configobj.ConfigObj(specpath, encoding='UTF8', list_values=False, _inspec=True)
-	#Load the config
-	config = configobj.ConfigObj(configpath, configspec=spec, create_empty=True, encoding='UTF8')
+	#Try to load the provided specification
+	try:
+		spec = ConfigObj(specpath, encoding='UTF8', list_values=False, _inspec=True)
+	except ParseError, IOError as e:
+		raise ConfError("Error loading specification %r: %r" %(specpath, e)
+	#Try to load the config
+	try:
+		config = ConfigObj(configpath, configspec=spec, create_empty=True, encoding='UTF8')
+	except ParseError as e: #No IOError because if we don't find the config file, we just create it from the spec
+		raiseConfError("Error when loading config %r" %(configpath))
 	#Now validate it...
-	val = validator.Validator()
+	val = Validator()
 	valid = config.validate(val, copy=True)
 	if valid == True:
 		#Config is valid, write it back to disk to make sure it has all values specified in the spec
