b54acd9d1fbb76011f1c01e3ee9a2cd2c21acc28
blake oliver <oliver22213@me.com>
2016-01-22 22:48:57 -0500

    Update text_options with the contents of options.py
    This should be a perfect working example of this annoying crash.
    It also includes an imbedded config dictionary mimicking the structure and types of the values in the real config files.
    Other developers, please feel free to toss it to anyone who you feel can fix this problem.

--- queriet/test_options.py
+++ queriet/test_options.py
@@ -1,6 +1,113 @@
 import wx
-from conf import GetConfig
-from options import Options
+import copy
+
+##########
+#The classes for the options dialog
+##########
+
+class Options(wx.Dialog):
+	def __init__(self,config, parent=None, title="options"):
+		super(Options, self).__init__(parent=parent, title=title)
+		self.original_config = config
+		self.config = copy.copy(config)
+		self.SetupUI()
+		self.Center()
+
+	def SetupUI(self):
+		self.panel = wx.Panel(self)
+		self.Sizer = wx.BoxSizer(wx.HORIZONTAL)
+		self.OptionsNB = OptionsNotebook(self.panel, self.config)
+		self.ok = wx.Button(self.panel, id=wx.ID_OK)
+		self.ok.Bind(wx.EVT_BUTTON, self.OnOk)
+		self.cancel = wx.Button(self.panel, id=wx.ID_CANCEL)
+		self.cancel.Bind(wx.EVT_BUTTON, self.CloseDialog)
+		self.Sizer.Add(self.OptionsNB, wx.EXPAND, 5)
+		self.Sizer.Add(self.ok, 0, wx.ALIGN_BOTTOM, 10)
+		self.Sizer.Add(self.cancel, 0, wx.ALIGN_BOTTOM, 10)
+		self.panel.SetSizer(self.Sizer)
+		self.Fit()
+		self.Layout()
+
+	def OnOk(self, e):
+		#Save the config
+		self.original_config = self.config
+		#End with the ok ID
+		self.EndModal(wx.ID_OK)
+
+	def CloseDialog(self, e):
+		#I shouldn't have to destroy the panels or anything
+		self.EndModal(wx.ID_CANCEL)
+
+
+class OptionsNotebook(wx.Notebook):
+	def __init__(self, parent, config):
+		super(OptionsNotebook, self).__init__(parent, id=wx.ID_ANY)
+		self.config = config
+		self.Setup()
+
+	def Setup(self):
+		self.general = GeneralPanel(self, self.config['general'])
+		self.logging = LoggingPanel(self, self.config['logging'])
+		self.AddPage(self.general, "General")
+		self.AddPage(self.logging, "Logging")
+		self.Bind(wx.EVT_NOTEBOOK_PAGE_CHANGED, self.OnPageChanged)
+
+	def OnPageChanged(self, e):
+		self.Layout()
+
+
+class OptionsPanel(wx.Panel):
+	def __init__(self, parent, config):
+		super(OptionsPanel, self).__init__(parent, id=wx.ID_ANY)
+		self.config = config
+		self.setup()
+
+	def setup(self):
+		pass
+
+class GeneralPanel(OptionsPanel):
+	def setup(self):
+		self.Sizer = wx.BoxSizer(wx.HORIZONTAL)
+		for k, v in self.config.iteritems():
+			if type(v)==bool:
+				checkbox = wx.CheckBox(self, label=k.replace("_", " "))
+				checkbox.SetValue(v)
+				checkbox.Bind(wx.EVT_CHECKBOX, self.toggle)
+				self.Sizer.Add(checkbox)
+		self.SetSizerAndFit(self.Sizer)
+
+	def toggle(self, e):
+		checkbox = e.GetEventObject()
+		self.config[checkbox.Label.replace(" ", "_")] = checkbox.GetValue()
+
+class LoggingPanel(OptionsPanel):
+	def setup(self):
+		self.Sizer = wx.GridBagSizer(10, 10)
+		LogLevels = ["debug", "info", "warning", "error", "critical"]
+		LogLevelLabel = wx.StaticText(self, label = "Logging level")
+		LogLevelListBox = wx.ListBox(self, choices=LogLevels)
+		LogLevelListBox.SetSelection(LogLevels.index(self.config['log_level']))
+		LogLevelListBox.Bind(wx.EVT_LISTBOX, self.OnListChange)
+		self.FormatLabel = wx.StaticText(self, label="Logging format")
+		self.FormatInput = wx.TextCtrl(self, value=self.config['format'])
+		self.FormatInput.Bind(wx.EVT_TEXT, self.FormatChange)
+		self.Sizer.Add(LogLevelLabel, pos=(1, 1))
+		self.Sizer.Add(LogLevelListBox, pos=(1, 2), span=(3, 1))
+		self.Sizer.Add(self.FormatLabel, pos=(5, 1))
+		self.Sizer.Add(self.FormatInput, pos=(5, 2), span=(1, 3))
+		self.SetSizerAndFit(self.Sizer)
+
+	def FormatChange(self, e):
+		config['format'] = e.GetEventObject().GetValue()
+
+	def OnListChange(self, e):
+		l = e.GetEventObject()
+		if l.GetSelection()>=0:
+			self.config['log_level'] = l.GetString(l.GetSelection())
+
+##########
+#Test interface
+##########
 
 class MainFrame(wx.Frame):
 	def __init__(self, parent, title):
@@ -11,14 +118,21 @@ class MainFrame(wx.Frame):
 
 	def setup(self):
 		panel = wx.Panel(self)
-		button = wx.Button(panel, label="Show Dialog")
-		button.Bind(wx.EVT_BUTTON, self.ShowDialog)
+		showbutton = wx.Button(panel, label="Show Dialog")
+		showbutton.Bind(wx.EVT_BUTTON, self.ShowDialog)
+		exitbutton = wx.Button(panel, label = "Exit")
+		exitbutton.Bind(wx.EVT_BUTTON, self.DoClose)
+		self.Bind(wx.EVT_CLOSE, self.DoClose)
+		self.config = {}
+		self.config['general'] = {'open_on_startup' : True, 'exit_to_tray' : True, 'ask_to_exit' : True}
+		self.config['logging'] = {'log_level' : 'debug', 'format' : '%(asctime)s - %(name)s - %(levelname)s: %(message)s'}
 
 	def ShowDialog(self, e):
-		config = GetConfig("queriet.confspec", "queriet.conf")
-		options = Options(config)
+		options = Options(self.config, parent=self)
 		options.ShowModal()
-		options.Destroy()
+
+	def DoClose(self, e):
+		wx.GetApp().ExitMainLoop()
 
 app = wx.App()
 mf = MainFrame(None, "Options test")
