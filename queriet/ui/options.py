1559863f5f3bdd5b9a53069dbbad5e49bbd7c0c1
Blake Oliver <oliver22213@me.com>
2016-02-03 11:36:30 -0500

    Work on the keybindings dialog a bit
    add docstrings, make methodnames more self explanatory, remove the "save config" button so it's in line with the other panels.
    Now, whenever a checkbox or the text field is changed, it calls on_binding_changed (the old on_brb thing), which updates the corresponding entry in the config file for the selected binding. This, of course, doesn't touch the actual config - just the local instance that the dialog has. It won't get modified until the OK button is pressed, and is discarded when the cancel button is pressed.

--- queriet/ui/options.py
+++ queriet/ui/options.py
@@ -24,16 +24,13 @@ class Options(wx.Dialog):
 		self.SetSizer(self.sizer)
 		self.Fit()
 		self.Layout()
-		#self.Show()
 
 	def OnOk(self, e):
 		self.SaveConfig()
-		#e.Skip()
 		self.EndModal(wx.ID_OK)
 
 	def CloseDialog(self, e):
 		#self.panel.Destroy()
-		#e.Skip()
 		self.EndModal(wx.ID_CANCEL)
 
 	def SaveConfig(self):
@@ -41,15 +38,18 @@ class Options(wx.Dialog):
 		self.original_config.write()
 
 class OptionsNotebook(wx.Notebook):
+	"""The main notebook that holds the entire options dialog and it's pages"""
+
 	def __init__(self, parent, config):
 		super(OptionsNotebook, self).__init__(parent, id=wx.ID_ANY)
 		self.config = config
 		self.Setup()
 
 	def Setup(self):
+		"""Sets up the options dialog and initializes each page"""
 		self.general = GeneralPanel(self, self.config['general'])
 		self.logging = LoggingPanel(self, self.config['logging'])
-		self.keybindings = KeystrokeEditor(self, self.config['keybindings'])
+		self.keybindings = KeybindingEditor(self, self.config['keybindings'])
 		self.AddPage(self.general, "General")
 		self.AddPage(self.logging, "Logging")
 		self.AddPage(self.keybindings, "Keybindings")
@@ -60,6 +60,7 @@ class OptionsNotebook(wx.Notebook):
 
 
 class OptionsPanel(wx.Panel):
+	"""A class that should be inherited by all classes that are notebook page objects"""
 	def __init__(self, parent, config):
 		super(OptionsPanel, self).__init__(parent, id=wx.ID_ANY)
 		self.config = config
@@ -68,7 +69,10 @@ class OptionsPanel(wx.Panel):
 	def setup(self):
 		pass
 
+
 class GeneralPanel(OptionsPanel):
+	"""The general tab panel"""
+
 	def setup(self):
 		"""Sets up the general tab panel. Currently, it consists of only boolean values, so a simple config dictionary iteration is all we need."""
 		self.sizer = wx.BoxSizer(wx.HORIZONTAL)
@@ -85,10 +89,14 @@ class GeneralPanel(OptionsPanel):
 		self.SetSizerAndFit(self.sizer)
 
 	def toggle(self, e):
+		"""Method that changes a config value relating to a specific checkbox. We need this because we don't explicitly name each one, and who wants to have specific handlers? That's just lame."""
 		checkbox = e.GetEventObject()
 		self.config[checkbox.Label.replace(" ", "_")] = checkbox.GetValue()
 
+
 class LoggingPanel(OptionsPanel):
+	"""Logging tab panel"""
+
 	def setup(self):
 		self.sizer = wx.GridBagSizer(10, 10)
 		LogLevels = ["debug", "info", "warning", "error", "critical"]
@@ -113,38 +121,64 @@ class LoggingPanel(OptionsPanel):
 		if l.GetSelection()>=0:
 			self.config['log_level'] = l.GetString(l.GetSelection())
 
-class KeystrokeEditor(OptionsPanel):
+
+class KeybindingEditor(OptionsPanel):
+	"""Tab in the options dialog that allows updating of keybindings""" 
+
 	#helper functions
+
 	def list_to_string(self, l):
+		"""Converts a list like:
+			['control', 'win', 'alt', 'space']
+			To:
+			control+win+alt+space
+		"""
 		return "+".join(l)
+
 	def string_to_list(self, string):
+		"""This splits a string like:
+			"control+win+alt+space"
+			To:
+			['control', 'win', 'alt', 'space]
+		"""
 		return string.split("+")
-	def pass_checklist(self, string):
+
+	def set_binding(self, string):
+		"""Sets up the checkboxes and text field for one keybinding"""
 		self.SetCheckboxes(self.string_to_list(string)[:-1])
-		self.Letter.SetValue(self.string_to_list(string)[-1:][0])
+		self.key.SetValue(self.string_to_list(string)[-1:][0])
+
 	def SetCheckboxes(self, l):
+		"""Sets 4 checkboxes: control, win, alt, and shift, based on a list passed.
+			The list should look like: ['control', 'shift', 'a']
+		"""
 		self.ControlCheckbox.SetValue(True if "control" in l else False)
 		self.WinCheckbox.SetValue(True if "win" in l else False)
 		self.ShiftCheckbox.SetValue(True if "shift" in l else False)
 		self.AltCheckbox.SetValue(True if "alt" in l else False)
+
 	def setup(self):
 		self.sizer = wx.BoxSizer(wx.HORIZONTAL)
 		self.OptionsListbox = wx.ListBox(self, choices=[])
-		self.OptionsListbox.Bind(wx.EVT_LISTBOX, self.on_change)
+		self.OptionsListbox.Bind(wx.EVT_LISTBOX, self.on_list_change)
 		self.sizer.Add(self.OptionsListbox)
 		self.ControlCheckbox=wx.CheckBox(self, label="Control")
 		self.WinCheckbox=wx.CheckBox(self, label="Windows")
 		self.AltCheckbox=wx.CheckBox(self, label="Alt")
 		self.ShiftCheckbox=wx.CheckBox(self, label="Shift")
-		self.Letter = wx.TextCtrl(self, -1)
-		self.Letter.SetMaxLength(1)
-		self.brb = wx.Button(self, label="Save config")
-		self.brb.Bind(wx.EVT_BUTTON, self.on_push_brb)
+		self.key = wx.TextCtrl(self, -1)
+		self.key.SetMaxLength(10)
+		#Set event handlers for the checkboxes
+		for item in [self.ControlCheckbox, self.WinCheckbox, self.AltCheckbox, self.ShiftCheckbox]:
+			item.Bind(wx.EVT_CHECKBOX, self.on_binding_updated)
+		#We have to bind the text field event separately because the event it emits (as well as the one we're looking for in the text field's case) is different from that of the checkbox
+		self.key.Bind(wx.EVT_TEXT, self.on_binding_updated)
+		#Add the modifier checkboxes and text field to the sizer
 		self.sizer.Add(self.ControlCheckbox)
 		self.sizer.Add(self.WinCheckbox)
 		self.sizer.Add(self.AltCheckbox)
 		self.sizer.Add(self.ShiftCheckbox)
-		self.sizer.Add(self.Letter)
+		self.sizer.Add(self.key)
 		#Same pattern from above code, but set checkboxes based on first value
 		count=0
 		for k, v in self.config.iteritems():
@@ -152,10 +186,16 @@ class KeystrokeEditor(OptionsPanel):
 			count += 1
 			if count==1:
 				self.OptionsListbox.SetSelection(0)
-				self.pass_checklist(self.config[k])
-	def on_change(self, e):
-		self.pass_checklist(self.config[self.OptionsListbox.GetString(self.OptionsListbox.GetSelection())])
-	def on_push_brb(self, e):
+				self.set_binding(self.config[k])
+
+	def on_list_change(self, e):
+		"""The event handler for when the listbox (and the focused keybinding), changes."""
+		self.set_binding(self.config[self.OptionsListbox.GetString(self.OptionsListbox.GetSelection())])
+
+	def on_binding_updated(self, e):
+		"""This method saves the currently focused binding back to config. It's called whenever any of the checkboxes are toggled or the text in the text field changes.
+			Note that the actual program configuration hasn't yet been modified - only the local instance of it. It will become the new configuration only if the OK button is pressed.
+		"""
 		l=[]
 		if self.ControlCheckbox.GetValue():
 			l.append("control")
@@ -165,6 +205,6 @@ class KeystrokeEditor(OptionsPanel):
 			l.append("win")
 		if self.ShiftCheckbox.GetValue():
 			l.append("shift")
-		l.append(self.Letter.GetValue())
+		l.append(self.key.GetValue())
 		f=self.list_to_string(l)
 		self.config[self.OptionsListbox.GetString(self.OptionsListbox.GetSelection())]=f
